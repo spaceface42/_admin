190c14ff010a7e5e274c179f0556a429466b07bb
graphis <ezsolt@gmail.com>
2026-06-06 18:49:18 +0200

    clean

--- src/js/16-prefill.js
+++
@@ -1,6 +0,0 @@
-/* ---------- prefill ---------- */
-(function init(){
-  const r=localStorage.getItem(LS_REPO), t=localStorage.getItem(LS_TOKEN);
-  if(r) el('repoUrl').value=r;
-  if(t){ try{ el('token').value=dec(t); }catch(e){} }
-})();
